b4f3c85cb9f92fc0b438a352a7e09389d0374424
Librarydks <a2motivation123456789@gmail.com>
2024-05-01 01:51:53 -0700

    server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,17 @@
-// Import the Express.js module
 const express = require('express');
-
-// Create an instance of the Express application
 const app = express();
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, world!'); 
+  res.send('<h1><center>WELCOME TO MYLIKI SERVER.......</br> version 1.0.0 </center> </H1>'); 
 });
 
 // Define other routes as needed...
 
 // Start the server
 const PORT = process.env.PORT || 3000; // Use the port specified in the environment variable PORT, or default to port 3000
+
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
